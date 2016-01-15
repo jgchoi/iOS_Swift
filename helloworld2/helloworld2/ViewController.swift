cbf6f8ac04be7f0f7bcddd71943c704e03a146e0
jgchoi <jgchoi@myseneca.ca>
2016-01-15 08:46:55 -0500

    Lab 1 - Part 2

--- helloworld2/helloworld2/ViewController.swift
+++ helloworld2/helloworld2/ViewController.swift
@@ -7,9 +7,28 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var firstTextField: UITextField!
+    let synth = AVSpeechSynthesizer()
+    var myUtterance = AVSpeechUtterance(string:"")
+    
+    
+    @IBAction func onButtonClick(sender: UIButton) {
+        firstTextField.canBecomeFirstResponder()
+        let color = UIColor.orangeColor()
+        nameLabel.textColor = color;
+        nameLabel.text = "Hello " + firstTextField.text!
+        myUtterance = AVSpeechUtterance(string: nameLabel.text!)
+        myUtterance.rate = 0.5
+        synth.speakUtterance(myUtterance)
+        firstTextField.resignFirstResponder()
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
