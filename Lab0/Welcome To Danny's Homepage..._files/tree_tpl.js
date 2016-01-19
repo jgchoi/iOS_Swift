daee5ae20ebea900d00c7e655f41226a2867754a
jgchoi <jgchoi@myseneca.ca>
2016-01-18 20:51:34 -0500

    Lab 0 Done

---
+++ Lab0/Welcome To Danny's Homepage..._files/tree_tpl.js
@@ -0,0 +1,54 @@
+/*
+	Feel free to use your custom icons for the tree. Make sure they
+	are all of the same size.
+	User icons collections are welcome, we'll publish them
+	giving all regards.
+*/
+
+var tree_tpl = {
+	'target'  : 'basefrm',  // name of the frame links will be opened in
+				// other possible values are: _blank, _parent, _search, _self and _top
+
+	'icon_e'  : 'icons/empty.gif', // empty image
+	// 'icon_l'  : 'icons/line.gif',  // vertical line
+	'icon_l'  : 'icons/empty.gif',  // vertical line
+
+	// 'icon_32' : 'icons/base.gif',   // root leaf icon normal
+	// 'icon_36' : 'icons/base.gif',   // root leaf icon selected
+	'icon_32' : 'icons/icon_tree.gif',   // root leaf icon normal
+	'icon_36' : 'icons/icon_tree.gif',   // root leaf icon selected
+
+	//'icon_48' : 'icons/base.gif',   // root icon normal
+	//'icon_52' : 'icons/base.gif',   // root icon selected
+	//'icon_56' : 'icons/base.gif',   // root icon opened
+	//'icon_60' : 'icons/base.gif',   // root icon selected
+	'icon_48' : 'icons/icon_tree.gif',   // root icon normal
+	'icon_52' : 'icons/icon_tree.gif',   // root icon selected
+	'icon_56' : 'icons/icon_tree.gif',   // root icon opened
+	'icon_60' : 'icons/icon_tree.gif',   // root icon selected
+	
+	'icon_16' : 'icons/cfb.gif', // node icon normal
+	'icon_20' : 'icons/ofb.gif', // node icon selected
+	'icon_24' : 'icons/ofb.gif', // node icon opened
+	'icon_28' : 'icons/ofb.gif', // node icon selected opened
+
+	'icon_0'  : 'icons/page.gif', // leaf icon normal
+	'icon_4'  : 'icons/page.gif', // leaf icon selected
+	
+	// original tree icons
+	// 'icon_2'  : 'icons/joinbottom.gif', // junction for leaf
+	// 'icon_3'  : 'icons/join.gif',       // junction for last leaf
+	// 'icon_18' : 'icons/plusbottom.gif', // junction for closed node
+	// 'icon_19' : 'icons/plus_.gif',       // junctioin for last closed node
+	// 'icon_26' : 'icons/minusbottom.gif',// junction for opened node
+	// 'icon_27' : 'icons/minus.gif'       // junctioin for last opended node
+
+	'icon_2'  : 'icons/empty.gif', // junction for leaf
+	'icon_3'  : 'icons/empty.gif',       // junction for last leaf
+	'icon_18' : 'icons/plus_ar.gif', // junction for closed node
+	'icon_19' : 'icons/plus_ar.gif',       // junctioin for last closed node
+	'icon_26' : 'icons/minus_ar.gif',// junction for opened node
+	'icon_27' : 'icons/minus_ar.gif'       // junctioin for last opended node
+
+};
+
